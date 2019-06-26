5529148f7875d2272f0131d477d3326360fd09ab
JM <woalsdl500@naver.com>
2019-06-27 01:08:09 +0900

    UPDATE word model

--- word/models.py
+++ word/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Word(models.Model):
+    word = models.CharField(max_length=100)
+    meaning = models.CharField(max_length=100)
+    example = models.CharField(max_length=100)
