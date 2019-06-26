2d8c777baa64fc470f6170a2d9c1fc6e0a540181
JM <woalsdl500@naver.com>
2019-06-27 01:03:19 +0900

    FIX settings.py timezone

--- web/settings.py
+++ web/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'Asia/Seoul'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
