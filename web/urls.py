b94c3456f40ae99887e23c0d74fa687d13dfd76c
lhju4e <today1186@naver.com>
2019-06-27 17:54:36 +0900

    UPDATE origin url

--- web/urls.py
+++ web/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('word.urls'))
+    path('',include('word.urls')),
+    path('file/',include('file_upload.urls'))
 ]
