dbddc91e6ca781a829450c879b37e17cabb4e905
lhju4e <today1186@naver.com>
2019-06-27 17:48:14 +0900

    UPDATE urls, template

---
+++ file_upload/urls.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 27 01:31:15 2019
+
+@author: JM, jisu
+"""
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.file_upload),
+]
