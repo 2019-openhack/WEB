a222d24b1fe3f62fbc5055c2cdb561f34783d7f4
lhju4e <today1186@naver.com>
2019-06-27 14:42:54 +0900

    view word

--- word/views.py
+++ word/views.py
@@ -6,3 +6,8 @@ def word_info(request):
     words = Word.objects.all()
     context = {'words':words}
     return render(request, 'word/word_info.html', context)
+
+def word_mean(request):
+    means = Word.objects.all()
+    context = {'means':means}
+    return render(request, 'word/word_mean.html', context)
