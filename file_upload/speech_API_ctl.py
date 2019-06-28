2ad869fb3b73bbb4c9dd9957eb56cc923ff91a12
jisuAn <ajs7270@naver.com>
2019-06-28 12:04:15 +0900

    fix speech api

--- file_upload/speech_API_ctl.py
+++ file_upload/speech_API_ctl.py
@@ -2,13 +2,8 @@ import io
 import os
 import sys
 
-# Imports the Google Cloud client library
-from google.cloud import speech
-from google.cloud.speech import enums
-from google.cloud.speech import types
 
-
-def tts(text):
+def tts(path,text):
     """Synthesizes speech from the input string of text."""
     from google.cloud import texttospeech
     client = texttospeech.TextToSpeechClient()
@@ -25,43 +20,14 @@ def tts(text):
         audio_encoding=texttospeech.enums.AudioEncoding.LINEAR16)
 
     response = client.synthesize_speech(input_text, voice, audio_config)
-
+    save_path = os.path.join(path, text+'.wav')
+    print(save_path)
     # The response's audio_content is binary.
-    with open(text + '.wav', 'wb') as out:
+    with open(save_path, 'wb') as out:
         out.write(response.audio_content)
-        print('"'+ text +'.wav"파일로 저장되었습니다.')
+        print('"'+ text +'.wav" is saved')
 
-
-# 현재 디렉토리에 있는 .raw 확장자를 가진 파일을 입력받고
-# 결과를 가져온다. 아직 리턴값은 정하지 않음
-def sst(audioFile):
-    # Instantiates a client
-    client = speech.SpeechClient()
-
-    # The name of the audio file to transcribe
-    file_name = os.path.join(
-        os.path.dirname(__file__),
-        audioFile)
-
-    # Loads the audio into memory
-    with io.open(file_name, 'rb') as audio_file:
-        content = audio_file.read()
-        audio = types.RecognitionAudio(content=content)
-
-    config = types.RecognitionConfig(
-        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=16000,
-        language_code='ko-KR')
-
-    # Detects speech in the audio file
-    response = client.recognize(config, audio)
-
-    return response.results
-    '''
-    for result in response.results:
-        print('Transcript: {}'.format(result.alternatives[0].transcript))
-    '''
 
 if __name__ == "__main__":
-    tts('타이머가 설정되었습니다.')
+    tts('jisu','apple')
     #sst('input.raw')
